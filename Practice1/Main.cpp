2fc74eb023a70af9bc14148cb4519f315e951802
곽민규 <mindol0320@naver.com>
2026-02-27 21:12:50 +0900

    boj 18352

---
+++ Practice1/Main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+struct City
+{
+	int id;
+	std::vector<int> subcities;
+};
+
+void BFS(std::vector<City> cities)
+{
+
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	int n, m, k, x;
+	std::cin >> n >> m >> k >> x;
+
+	std::vector<City> cities;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cities.emplace_back(i, std::vector<int>());
+	}
+
+	for (int i = 0; i < m; ++i)
+	{
+		int a, b;
+		std::cin >> a >> b;
+		cities[a - 1].subcities.emplace_back(b - 1);
+	}
+
+	for (City city : cities)
+	{
+		if (city.id == x - 1)
+		{
+			while (k > 0)
+			{
+				BFS(cities);
+				--k;
+			}
+		}
+	}
+
+	return 0;
+}
